dc033dda02510d35bbd9e3274a3840764aefc73e
pettai <pettai@sunet.se>
2022-04-11 13:25:29 +0200

    Update MSUIAPIClient.py
    add parameters to POST as well

--- examples/MSUIAPIClient.py
+++ examples/MSUIAPIClient.py
@@ -28,12 +28,12 @@ import urllib.parse
 # Do a GET request.  Example: Get a list of users,
 # and limit output to 10 user, and sort on username 
 #
-# getusers = api.do_get('/xhr/user', {'sort[limit]': '10', 'sort[order]': 'username'})
+# listusers = api.do_get('/xhr/user', {'sort[limit]': '10', 'sort[order]': 'username'})
 #
 # Do a POST request.  Example: Add a user named "foobar", 
 # with role_id 4 (eg. End-user), to domain mydomain.name with password auth disabled
 #
-# adduser = api.do_post('/xhr/user', {'username' : 'foobar', 'role_id': 4, 'domain': 'mydomain.name', 'password': ''})
+# adduser = api.do_post('/xhr/user', {'username' : 'foobar', 'role_id': 4, 'domain': 'mydomain', 'password': ''})
 #
 # Do a PATCH request.  Example: Update user id 123, 
 # to get role_id: 3 (eg. Realm-admin)
@@ -83,11 +83,18 @@ class MSUIAPIClient:
             full_url += '?' + urllib.parse.urlencode(params)
 
         result = self.session.get(full_url)
-        return result.json()
+        return result
 
-    def do_post(self, rel_url, post_data):
+    def do_post(self, rel_url, post_data, params=None):
         """Do a POST request against the API server. post_data should be a dictionary."""
-        result = self.session.post(self.url + rel_url, json=post_data)
+        full_url = self.url + rel_url
+
+        # Add params
+        if type(params) is dict:
+            print(full_url, '?', params)
+            full_url += '?' + urllib.parse.urlencode(params)
+
+        result = self.session.post(full_url, json=post_data)
         if result.status_code == 200:
             return result.json()
         else:
